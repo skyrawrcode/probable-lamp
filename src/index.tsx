911d51a3e725e7fcb5104e32d3ba1483b006e3d4
Cody Droz <cody.droz@gmail.com>
2020-12-19 08:57:23 -0600

    netlify authentication

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import netlifyIdentity from 'netlify-identity-widget';
 
+(window as any).netlifyIdentity = netlifyIdentity;
+// You must run this once before trying to interact with the widget
+
+netlifyIdentity.init();
 ReactDOM.render(
   <React.StrictMode>
     <App />
